750db858e295b5f2713ad4bbdc4cc2ef5d60562b
TaeHee00 <kancth03@gmail.com>
2023-10-02 23:30:46 +0900

    init: springboot 프로젝트 파일 생성

---
+++ src/test/java/com/kancth03/GoodNightHackathonSpringBoot/GoodNightHackathonSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kancth03.GoodNightHackathonSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodNightHackathonSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
