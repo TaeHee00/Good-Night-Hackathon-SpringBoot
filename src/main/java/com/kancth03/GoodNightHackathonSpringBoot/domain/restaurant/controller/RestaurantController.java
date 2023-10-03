fdb04e590fe8e27b84a690804097b1aba8d2fbfa
SNOW <kancth03@gmail.com>
2023-10-04 04:43:29 +0900

    feat: RestaurantController 파일 생성

---
+++ src/main/java/com/kancth03/GoodNightHackathonSpringBoot/domain/restaurant/controller/RestaurantController.java
@@ -0,0 +1,9 @@
+package com.kancth03.GoodNightHackathonSpringBoot.domain.restaurant.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestaurantController {
+
+
+}
