a45b6416e684538eb294ae2e37d776ac97b287f6
SNOW <kancth03@gmail.com>
2023-10-04 01:57:18 +0900

    feat: Review Entity 생성

---
+++ src/main/java/com/kancth03/GoodNightHackathonSpringBoot/domain/review/entity/Review.java
@@ -0,0 +1,25 @@
+package com.kancth03.GoodNightHackathonSpringBoot.domain.review.entity;
+
+import com.kancth03.GoodNightHackathonSpringBoot.domain.restaurant.entity.Restaurant;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Getter
+public class Review {
+
+    @Id
+    @Column(nullable = false, updatable = false, name = "review_id")
+    private Long id;
+
+    @Column(nullable = false, length = 300)
+    private String title;
+    @Column(nullable = false, length = 10000)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant_id;
+}
