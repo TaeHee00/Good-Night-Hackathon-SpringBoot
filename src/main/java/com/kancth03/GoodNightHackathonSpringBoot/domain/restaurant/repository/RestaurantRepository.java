b4cbc90dbc68fb24b1cd269481427a8c25220619
SNOW <kancth03@gmail.com>
2023-10-04 02:38:58 +0900

    fix: JPA Entity, PK 위치를 잘못입력해서 수정

--- src/main/java/com/kancth03/GoodNightHackathonSpringBoot/domain/restaurant/repository/RestaurantRepository.java
+++ src/main/java/com/kancth03/GoodNightHackathonSpringBoot/domain/restaurant/repository/RestaurantRepository.java
@@ -2,8 +2,6 @@ package com.kancth03.GoodNightHackathonSpringBoot.domain.restaurant.repository;
 
 import com.kancth03.GoodNightHackathonSpringBoot.domain.restaurant.entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface RestaurantRepository extends JpaRepository<Long, Restaurant> {
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 }
