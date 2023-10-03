1beb7c4ea18d1520832b94f080662bfaaaa1d933
SNOW <kancth03@gmail.com>
2023-10-04 05:34:03 +0900

    feat: Restaurant 엔티티에 등록일 추가

--- src/main/java/com/kancth03/GoodNightHackathonSpringBoot/domain/restaurant/entity/Restaurant.java
+++ src/main/java/com/kancth03/GoodNightHackathonSpringBoot/domain/restaurant/entity/Restaurant.java
@@ -2,11 +2,17 @@ package com.kancth03.GoodNightHackathonSpringBoot.domain.restaurant.entity;
 
 import com.kancth03.GoodNightHackathonSpringBoot.domain.review.entity.Review;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Table
 @Getter
@@ -25,6 +31,9 @@ public class Restaurant {
 
     @OneToMany(mappedBy = "restaurant_id")
     private List<Review> reviews = new ArrayList<>();
+
+    @CreatedDate
+    private LocalDateTime createAt;
 
     private Restaurant(String name, RestaurantCategory category) {
         this.name = name;
