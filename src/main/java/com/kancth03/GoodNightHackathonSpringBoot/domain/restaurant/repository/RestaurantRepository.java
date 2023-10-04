7078e258a9b557c565b0e08b807bca9b4ad1f100
SNOW <kancth03@gmail.com>
2023-10-05 05:32:10 +0900

    feat: 레스토랑 목록 조회 구현을 위해 Jpa 메서드 생성

--- src/main/java/com/kancth03/GoodNightHackathonSpringBoot/domain/restaurant/repository/RestaurantRepository.java
+++ src/main/java/com/kancth03/GoodNightHackathonSpringBoot/domain/restaurant/repository/RestaurantRepository.java
@@ -1,11 +1,14 @@
 package com.kancth03.GoodNightHackathonSpringBoot.domain.restaurant.repository;
 
 import com.kancth03.GoodNightHackathonSpringBoot.domain.restaurant.entity.Restaurant;
+import com.kancth03.GoodNightHackathonSpringBoot.domain.restaurant.entity.RestaurantCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
     Optional<Restaurant> findByName(String restaurantName);
+    List<Restaurant> findAllByCategory(RestaurantCategory category);
 }
