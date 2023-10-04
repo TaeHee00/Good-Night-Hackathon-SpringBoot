7802865c23df3138c686e1372039addbd6fb8bb2
SNOW <kancth03@gmail.com>
2023-10-05 06:28:11 +0900

    feat: 리뷰 수정 기능 구현

--- src/main/java/com/kancth03/GoodNightHackathonSpringBoot/domain/review/controller/ReviewController.java
+++ src/main/java/com/kancth03/GoodNightHackathonSpringBoot/domain/review/controller/ReviewController.java
@@ -2,6 +2,8 @@ package com.kancth03.GoodNightHackathonSpringBoot.domain.review.controller;
 
 import com.kancth03.GoodNightHackathonSpringBoot.domain.review.dto.AddReviewRequest;
 import com.kancth03.GoodNightHackathonSpringBoot.domain.review.dto.AddReviewResponse;
+import com.kancth03.GoodNightHackathonSpringBoot.domain.review.dto.ModifyReviewRequest;
+import com.kancth03.GoodNightHackathonSpringBoot.domain.review.dto.ModifyReviewResponse;
 import com.kancth03.GoodNightHackathonSpringBoot.domain.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -18,5 +20,12 @@ public class ReviewController {
     public AddReviewResponse addReview(@PathVariable Long restaurantId,
                                        @RequestBody AddReviewRequest request) {
         return reviewService.addReview(restaurantId, request);
+    }
+
+    // 리뷰 수정
+    @PutMapping("/{reviewId}")
+    public ModifyReviewResponse modifyReview(@PathVariable Long reviewId,
+                                             @RequestBody ModifyReviewRequest request) {
+        return reviewService.modifyReview(reviewId, request);
     }
 }
--- src/main/java/com/kancth03/GoodNightHackathonSpringBoot/domain/review/service/ReviewService.java
+++ src/main/java/com/kancth03/GoodNightHackathonSpringBoot/domain/review/service/ReviewService.java
@@ -4,6 +4,8 @@ import com.kancth03.GoodNightHackathonSpringBoot.domain.restaurant.entity.Restau
 import com.kancth03.GoodNightHackathonSpringBoot.domain.restaurant.repository.RestaurantRepository;
 import com.kancth03.GoodNightHackathonSpringBoot.domain.review.dto.AddReviewRequest;
 import com.kancth03.GoodNightHackathonSpringBoot.domain.review.dto.AddReviewResponse;
+import com.kancth03.GoodNightHackathonSpringBoot.domain.review.dto.ModifyReviewRequest;
+import com.kancth03.GoodNightHackathonSpringBoot.domain.review.dto.ModifyReviewResponse;
 import com.kancth03.GoodNightHackathonSpringBoot.domain.review.entity.Review;
 import com.kancth03.GoodNightHackathonSpringBoot.domain.review.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
@@ -22,5 +24,19 @@ public class ReviewService {
         Review review = Review.of(request.title(), request.content(), restaurant);
 
         return AddReviewResponse.entityToDto(reviewRepository.save(review));
+    }
+
+    public ModifyReviewResponse modifyReview(Long reviewId, ModifyReviewRequest request) {
+        Review review = reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new IllegalArgumentException("No review exists that matches Id."));
+
+        if (request.title() != null) {
+            review.setTitle(request.title());
+        }
+        if (request.content() != null) {
+            review.setContent(request.content());
+        }
+
+        return ModifyReviewResponse.entityToDto(reviewRepository.save(review));
     }
 }
